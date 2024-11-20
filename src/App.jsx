f1d8c9fa67a03375221e51b9f348b91e3ad5252d
QuangDev <ng.quang.coder.new@gmail.com>
2024-11-20 20:49:48 +0700

    Build_basic_layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import Button from "@mui/material/Button"
-import { AccessAlarm, ThreeDRotation } from "@mui/icons-material"
-import Typography from "@mui/material/Typography"
 import { useColorScheme } from "@mui/material/styles"
 import Box from "@mui/material/Box"
 import InputLabel from "@mui/material/InputLabel"
@@ -10,6 +7,7 @@ import Select from "@mui/material/Select"
 import LightModeIcon from "@mui/icons-material/LightMode"
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined"
 import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness"
+import Container from "@mui/material/Container"
 
 function ModeToggle() {
   const { mode, setMode } = useColorScheme()
@@ -17,63 +15,79 @@ function ModeToggle() {
     setMode(event.target.value)
   }
   return (
-    <Box sx={{ minWidth: 120 }}>
-      <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-        <InputLabel id="dark-mode-select-label">Mode</InputLabel>
-        <Select
-          labelId="dark-mode-select-label"
-          id="dark-mode-select"
-          value={mode}
-          label="Mode"
-          onChange={handleChange}>
-          <MenuItem value="light">
-            <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
-              <LightModeIcon fontSize="small" />
-              Light
-            </div>
-          </MenuItem>
-          <MenuItem value="dark">
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-                gap: "8px",
-              }}>
-              <DarkModeOutlinedIcon fontSize="small" />
-              Dark
-            </div>
-          </MenuItem>
-          <MenuItem value="system">
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-                gap: "8px",
-              }}>
-              <SettingsBrightnessIcon fontSize="small" />
-              System
-            </div>
-          </MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="dark-mode-select-label">Mode</InputLabel>
+      <Select
+        labelId="dark-mode-select-label"
+        id="dark-mode-select"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}>
+        <MenuItem value="light">
+          <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+            <LightModeIcon fontSize="small" />
+            Light
+          </div>
+        </MenuItem>
+        <MenuItem value="dark">
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "8px",
+            }}>
+            <DarkModeOutlinedIcon fontSize="small" />
+            Dark
+          </div>
+        </MenuItem>
+        <MenuItem value="system">
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "8px",
+            }}>
+            <SettingsBrightnessIcon fontSize="small" />
+            System
+          </div>
+        </MenuItem>
+      </Select>
+    </FormControl>
   )
 }
 function App() {
   return (
     <>
-      <hr></hr>
-      <ModeToggle />
-      <hr></hr>
-      <div>QuangNguyenDev</div>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <br></br>
-      <AccessAlarm />
-      <ThreeDRotation />
-      <br></br>
-      <Typography color="text.secondary">Test Typography</Typography>
+      <Container disableGutters maxWidth={false} sx={{ height: "100vh" }}>
+        <Box
+          sx={{
+            backgroundColor: "primary.light",
+            height: (theme) => theme.trello.appBarHeight,
+            display: "flex",
+            alignItems: "center",
+          }}>
+          <ModeToggle />
+        </Box>
+        <Box
+          sx={{
+            backgroundColor: "primary.dark",
+            height: (theme) => theme.trello.boardBarHeight,
+            display: "flex",
+            alignItems: "center",
+          }}>
+          Board Bar
+        </Box>
+        <Box
+          sx={{
+            backgroundColor: "primary.main",
+            height: (theme) =>
+              `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
+            display: "flex",
+            alignItems: "center",
+          }}>
+          Board Content
+        </Box>
+      </Container>
     </>
   )
 }
