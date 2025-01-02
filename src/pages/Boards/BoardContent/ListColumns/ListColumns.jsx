a1efc806890af8e75261bbbefa44f7fc415236d3
QuangDev <ng.quang.coder.new@gmail.com>
2025-01-02 23:19:11 +0700

    Drag-drop-columns

--- src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
+++ src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -3,45 +3,53 @@ import Box from "@mui/material/Box"
 import Column from "./Column/Column"
 import Button from "@mui/material/Button"
 import NoteAddIcon from "@mui/icons-material/NoteAdd"
+import {
+  SortableContext,
+  horizontalListSortingStrategy,
+} from "@dnd-kit/sortable"
 
 const ListColumns = ({ columns }) => {
   return (
-    <Box
-      sx={{
-        bgcolor: "inherit",
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        overflowY: "hidden",
-        overflowX: "auto",
-        "&::-webkit-scrollbar-track": { m: 2 },
-      }}>
-      {columns?.map((column) => {
-        return <Column key={column?._id} column={column} />
-      })}
-
+    <SortableContext
+      items={columns.map((c) => c._id)}
+      strategy={horizontalListSortingStrategy}>
       <Box
         sx={{
-          bgcolor: "#ffffff3d",
-          height: "fit-content",
-          borderRadius: "6px",
-          mx: 2,
-          maxWidth: 200,
-          minWidth: 200,
+          bgcolor: "inherit",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          overflowY: "hidden",
+          overflowX: "auto",
+          "&::-webkit-scrollbar-track": { m: 2 },
         }}>
-        <Button
-          startIcon={<NoteAddIcon />}
+        {columns?.map((column) => {
+          return <Column key={column?._id} column={column} />
+        })}
+
+        <Box
           sx={{
-            color: "white",
-            pl: 2.5,
-            py: 1,
-            justifyContent: "flex-start",
-            width: "100%",
+            bgcolor: "#ffffff3d",
+            height: "fit-content",
+            borderRadius: "6px",
+            mx: 2,
+            maxWidth: 200,
+            minWidth: 200,
           }}>
-          Add new column
-        </Button>
+          <Button
+            startIcon={<NoteAddIcon />}
+            sx={{
+              color: "white",
+              pl: 2.5,
+              py: 1,
+              justifyContent: "flex-start",
+              width: "100%",
+            }}>
+            Add new column
+          </Button>
+        </Box>
       </Box>
-    </Box>
+    </SortableContext>
   )
 }
 
