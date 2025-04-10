57ff24c990a1edb609eaf3de273c0e9b57475868
QuangDev <ng.quang.coder.new@gmail.com>
2025-04-10 11:34:27 +0700

    call_api_create_column_card

--- src/assets/apis/index.js
+++ src/assets/apis/index.js
@@ -6,3 +6,15 @@ export const fetchBoardDetailsAPI = async (boardId) => {
 
   return response.data
 }
+
+export const createColumnAPI = async (newColumnData) => {
+  const response = await axios.post(`${API_ROOT}/v1/columns/`, newColumnData)
+
+  return response.data
+}
+
+export const createCardAPI = async (newCardData) => {
+  const response = await axios.post(`${API_ROOT}/v1/cards/`, newCardData)
+
+  return response.data
+}
--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,8 +3,14 @@ import Container from "@mui/material/Container"
 import AppBar from "~/components/AppBar/AppBar"
 import BoardBar from "./BoardBar/BoardBar"
 import BoardContent from "./BoardContent/BoardContent"
-import { mockData } from "~/assets/apis/mock-data"
-import { fetchBoardDetailsAPI } from "~/assets/apis"
+// import { mockData } from "~/assets/apis/mock-data"
+import { generatePlaceHolderCard } from "~/utils/formatters"
+import { isEmpty } from "lodash"
+import {
+  fetchBoardDetailsAPI,
+  createColumnAPI,
+  createCardAPI,
+} from "~/assets/apis"
 
 function Board() {
   const [board, setBoard] = useState(null)
@@ -12,16 +18,57 @@ function Board() {
   useEffect(() => {
     const boardId = "67f33b0b53427a8ea6338637"
     fetchBoardDetailsAPI(boardId).then((board) => {
+      // xử lý kéo thả card trong 1 column rỗng
+      board.columns.forEach((column) => {
+        if (isEmpty(column.cards)) {
+          column.cards = [generatePlaceHolderCard(column)]
+          column.cardOrderIds = [generatePlaceHolderCard(column)._id]
+        }
+      })
       setBoard(board)
     })
   }, [])
 
-  console.log("board: ", board)
+  const createNewColumn = async (newColumnTitle) => {
+    const newColumn = await createColumnAPI({
+      title: newColumnTitle,
+      boardId: board._id,
+    })
+    // Xử lý kéo thả card trong 1 column rỗng
+    newColumn.cards = [generatePlaceHolderCard(newColumn)]
+    newColumn.cardOrderIds = [generatePlaceHolderCard(newColumn)._id]
+    // update state board
+    const newBoard = { ...board }
+    newBoard.columns.push(newColumn)
+    newBoard.columnOrderIds.push(newColumn._id)
+    setBoard(newBoard)
+  }
+  const createNewCard = async (newCardData) => {
+    const newCard = await createCardAPI({
+      title: newCardData.title,
+      columnId: newCardData.columnId,
+      boardId: board._id,
+    })
+
+    const newBoard = { ...board }
+    const updatedColumn = newBoard.columns.find(
+      (column) => column._id === newCard.columnId,
+    )
+    if (updatedColumn) {
+      updatedColumn.cards.push(newCard)
+      updatedColumn.cardOrderIds.push(newCard._id)
+    }
+    setBoard(newBoard)
+  }
   return (
     <Container disableGutters maxWidth={false} sx={{ height: "100vh" }}>
       <AppBar />
-      <BoardBar board={mockData?.board} />
-      <BoardContent board={mockData?.board} />
+      <BoardBar board={board} />
+      <BoardContent
+        board={board}
+        createNewColumn={createNewColumn}
+        createNewCard={createNewCard}
+      />
     </Container>
   )
 }
