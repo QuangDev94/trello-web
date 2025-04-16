beb25a837181eff390c6852e719ccc8eda584c51
QuangDev <ng.quang.coder.new@gmail.com>
2025-04-16 08:04:14 +0700

    fix BUILD_MODE dev and prod

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,12 @@
-// export const API_ROOT = "http://localhost:8017"
+/* eslint-disable no-undef */
 
-export const API_ROOT = "https://backend-trello-api.onrender.com"
+let apiRoot = ""
+console.log(process.env)
+if (process.env.BUILD_MODE === "dev") {
+  apiRoot = "http://localhost:8017"
+}
+if (process.env.BUILD_MODE === "prod") {
+  apiRoot = "https://backend-trello-api.onrender.com"
+}
+
+export const API_ROOT = apiRoot
