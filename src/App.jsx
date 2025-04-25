fd9c84fd60ee25bf61c035f5e1ff0f6b99bd8f3b
QuangDev <ng.quang.coder.new@gmail.com>
2025-04-25 10:39:38 +0700

    HttpOnly-cookie_AuthMiddleware_Redux-persist_Protected-routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import Board from "~/pages/Boards/_id"
-import { Routes, Route, Navigate } from "react-router-dom"
+import { Routes, Route, Navigate, Outlet } from "react-router-dom"
 import NotFound from "./pages/404/NotFound"
 import Auth from "./pages/Auth/Auth"
 import AccountVerification from "./pages/Auth/AccountVerification"
+import { useSelector } from "react-redux"
+import { selectCurrentUser } from "./redux/features/userSlice"
 
+// eslint-disable-next-line react/prop-types
+const ProtectedRouter = ({ user }) => {
+  if (!user) return <Navigate to="/login" replace={true} />
+
+  return <Outlet />
+}
 function App() {
+  const currentUser = useSelector(selectCurrentUser)
   return (
     <Routes>
       <Route
@@ -17,7 +26,11 @@ function App() {
           <Navigate to="/boards/67f33b0b53427a8ea6338637" replace={true} />
         }
       />
-      <Route path="/boards/:boardId" element={<Board />} />
+      {/* Protected Routes (Chỉ cho truy cập vào những route bên trong khi đã login) */}
+      <Route element={<ProtectedRouter user={currentUser} />}>
+        {/* Outlet là những child route trong Protected Routes */}
+        <Route path="/boards/:boardId" element={<Board />} />
+      </Route>
       {/* Authentication */}
       <Route path="/login" element={<Auth />} />
       <Route path="/register" element={<Auth />} />
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,31 @@ import { configureStore } from "@reduxjs/toolkit"
 import activeBoardReducer from "./features/activeBoardSlice"
 import userReducer from "./features/userSlice"
 
+import { combineReducers } from "@reduxjs/toolkit"
+import { persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage"
+
+// Cấu hình persist
+const rootPersistConfig = {
+  // key của persist do ta chỉ định
+  key: "root",
+  // Biến storage ở trên - lưu vào localstorage
+  storage: storage,
+  // Định nghĩa các slice dữ liệu được phép duy trì qua mỗi lần F5 trình duyệt
+  whiteList: ["user"],
+}
+// Combine các reduces trong dự án
+const reducers = combineReducers({
+  activeBoard: activeBoardReducer,
+  user: userReducer,
+})
+
+// Thực hiện persist Reducer
+const persistedReducers = persistReducer(rootPersistConfig, reducers)
+
 export const store = configureStore({
-  reducer: { activeBoard: activeBoardReducer, user: userReducer },
+  reducer: persistedReducers,
+  // fix warning error when implement redux-persist
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 })
